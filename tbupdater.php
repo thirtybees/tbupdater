0faa54126e8de178b9bdb0aed6662e72763a33a2
Michael Dekker <git@michaeldekker.nl>
2017-04-28 00:26:59 +0200

    Bump to 1.1.2

--- tbupdater.php
+++ tbupdater.php
@@ -76,7 +76,7 @@ class TbUpdater extends Module
     {
         $this->name = 'tbupdater';
         $this->tab = 'administration';
-        $this->version = '1.1.1';
+        $this->version = '1.1.2';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 1;
