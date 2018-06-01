fef8f3750d3cb5a6a8182710d45ec83d30c9dac5
Markus Hitter <mah@jump-ing.de>
2018-06-01 12:41:00 +0200

    Make headers in PHP files match the template.

--- classes/autoload.php
+++ classes/autoload.php
@@ -5,7 +5,7 @@
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.md
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
@@ -14,7 +14,7 @@
  *
  * @author    thirty bees <modules@thirtybees.com>
  * @copyright 2017-2018 thirty bees
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @license   Academic Free License (AFL 3.0)
  */
 
 if (!function_exists('TbUpdaterModule\GuzzleHttp\uri_template')) {
--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * 2007-2016 PrestaShop
- *
- * thirty bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
  * Copyright (C) 2017-2018 thirty bees
+ * Copyright (C) 2007-2016 PrestaShop SA
+ *
+ * thirty bees is an extension to the PrestaShop software by PrestaShop SA.
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
@@ -19,8 +19,8 @@
  * @author    PrestaShop SA <contact@prestashop.com>
  * @copyright 2017-2018 thirty bees
  * @copyright 2007-2016 PrestaShop SA
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ * @license   Academic Free License (AFL 3.0)
+ * PrestaShop is an internationally registered trademark of PrestaShop SA.
  */
 
 require_once __DIR__.'/classes/autoload.php';
