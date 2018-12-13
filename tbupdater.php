8bff083a3aca06230bb0ebed4e82c52f47ac4e1c
Markus Hitter <mah@jump-ing.de>
2018-12-13 17:33:19 +0100

    Make release 1.4.0.

--- tbupdater.php
+++ tbupdater.php
@@ -78,7 +78,7 @@ class TbUpdater extends Module
     {
         $this->name = 'tbupdater';
         $this->tab = 'administration';
-        $this->version = '1.3.4';
+        $this->version = '1.4.0';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 1;
