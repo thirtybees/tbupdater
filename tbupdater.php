9b8476a1d9df621b49ee41e37d11a5f902efc566
Petr Hučík <petr@getdatakick.com>
2022-10-18 09:35:06 +0200

    Release tbupdater v1.5.1

--- tbupdater.php
+++ tbupdater.php
@@ -78,7 +78,7 @@ class TbUpdater extends Module
     {
         $this->name = 'tbupdater';
         $this->tab = 'administration';
-        $this->version = '1.5.0';
+        $this->version = '1.5.1';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 1;
